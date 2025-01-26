04ebc67e22f14ce67e6cff9542ee4d7be957300a
kalbar-news <usutoto11@gmail.com>
2025-01-26 18:47:37 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$config = array(
+  	'url'			=> 'https://kalbar-news.github.io/news/',
+	'folder'  		=> 'folder.txt',
+	'template'  	=> 'temp.html',
+	'keywords'  	=> 'kw.txt',
+	'sitemap'		=> 'sitemap.xml',
+	'emot' 			=> 'emot.txt',
+	'title' 		=> 'title.txt',
+	'description' 	=> 'description.txt',
+);
+?>
